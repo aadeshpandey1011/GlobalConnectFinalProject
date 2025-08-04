0f20b38b12478ce15f4f67de7b678efe81b77396
Rakesh-De-Coder <rakeshofficial9451@gmail.com>
2025-08-04 22:20:34 +0530

    Post ,Likes & Dislike api End Point Added

---
+++ Global-Connect-Backend/controller/post.js
@@ -0,0 +1,60 @@
+const PostModel = require('../models/post');
+
+
+
+exports.addPost = async (req, res) => {
+    try {
+        const { desc, imageLink } = req.body;
+        let userId = req.user._id;
+
+        const addPost = new PostModel({ user: userId, desc, imageLink })
+        if(!addPost){
+            return res.status(400).json({error:"Something Went Wrong"})
+        }
+        await addPost.save()
+        return res.status(200).json({
+            message:"Posted Successfully",
+            post:addPost
+        })
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Server error', message: err.message });
+    }
+}
+
+exports.likeDislikePost = async (req, res) => {
+    try {
+        let selfId = req.user._id;
+        let { postId } = req.body;
+        let post = await PostModel.findById(postId);
+        if(!post){
+            return res.status(400).json({error:'No Such Post Found'})
+        }
+        const index = post.likes.findIndex(id=>id.equals(selfId));
+
+        if(index!==-1){
+            //User Already Liked The Post Remove Like
+            post.likes.splice(index,1);
+
+        }else{
+            //User not liked Yet Add Likes
+            post.likes.push(selfId)
+        }
+        await post.save()
+        res.status(200).json({
+            message:index!==-1?"Post Disliked":"Post Liked",
+            likes:post.likes
+        })
+        
+
+        await post.save();
+        res.status(200).json({
+            message: index !== -1 ? 'Post unliked' : 'Post liked',
+            likes: post.likes
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Server error', message: err.message });
+    }
+}
--- Global-Connect-Backend/index.js
+++ Global-Connect-Backend/index.js
@@ -14,9 +14,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 const UserRoutes = require('./routes/user');
+const PostRoutes = require('./routes/post');
+
 
 
 app.use('/api/auth',UserRoutes);
+app.use('/api/post',PostRoutes);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
