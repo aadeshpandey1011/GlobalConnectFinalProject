ec24582ab8520241231aca918ba728c1c2776951
aadeshpandey1011 <2002.aman.p@gmail.com>
2025-08-12 23:23:08 +0530

    Update navbar2.jsx with link

--- Global-Connect-frontend/src/components/Navbar2/navbar2.jsx
+++ Global-Connect-frontend/src/components/Navbar2/navbar2.jsx
@@ -39,7 +39,7 @@ const Navbar2 = () => {
 
 
     const searchAPICall = async () => {
-        await axios.get(`http://localhost:4000/api/auth/findUser?query=${debouncedTerm}`, { withCredentials: true }).then(res => {
+        await axios.get(`https://globalconnectfinalproject.onrender.com/api/auth/findUser?query=${debouncedTerm}`, { withCredentials: true }).then(res => {
             console.log(res)
 
             setSearchUser(res.data.users)
@@ -51,7 +51,7 @@ const Navbar2 = () => {
     }
 
     const fetchNotification = async () => {
-        await axios.get('http://localhost:4000/api/notification/activeNotification', { withCredentials: true }).then(res => {
+        await axios.get('https://globalconnectfinalproject.onrender.com/notification/activeNotification', { withCredentials: true }).then(res => {
             var count = res.data.count;
             setNotificationCount(count)
         }).catch(err => {
