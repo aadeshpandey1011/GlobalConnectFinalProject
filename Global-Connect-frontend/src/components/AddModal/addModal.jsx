7b3aa4050bf902567d20922bd85a09987e6d7013
aadeshpandey1011 <2002.aman.p@gmail.com>
2025-08-12 23:14:54 +0530

    Update addModal.jsx with link

--- Global-Connect-frontend/src/components/AddModal/addModal.jsx
+++ Global-Connect-frontend/src/components/AddModal/addModal.jsx
@@ -14,7 +14,7 @@ const AddModal = (props) => {
     const handlePost = async () => {
         if (desc.trim().length === 0 & !imageUrl) return toast.error("Please enter any field");
 
-        await axios.post('http://localhost:4000/api/post',{desc:desc,imageLink:imageUrl},{withCredentials:true}).then((res=>{
+        await axios.post('https://globalconnectfinalproject.onrender.com/api/post',{desc:desc,imageLink:imageUrl},{withCredentials:true}).then((res=>{
             window.location.reload();
         })).catch(err => {
             console.log(err)
