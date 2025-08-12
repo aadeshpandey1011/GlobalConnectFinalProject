b83fc84af05c4149c772a92323fb9b67b99c4ef2
aadeshpandey1011 <2002.aman.p@gmail.com>
2025-08-12 23:39:53 +0530

    Update notification.jsx with link

--- Global-Connect-frontend/src/pages/Notification/notification.jsx
+++ Global-Connect-frontend/src/pages/Notification/notification.jsx
@@ -105,7 +105,7 @@ const Notification = () => {
   const [notifications, setNotifications] = useState([]);
 
   const fetchNotificationData = async () => {
-    await axios.get('http://localhost:4000/api/notification', { withCredentials: true }).then(res => {
+    await axios.get('https://globalconnectfinalproject.onrender.com/api/notification', { withCredentials: true }).then(res => {
       console.log(res.data.notifications)
       setNotifications(res.data.notifications)
 
@@ -116,7 +116,7 @@ const Notification = () => {
   }
 
   const handleOnClickNotification = async (item) => {
-    await axios.put('http://localhost:4000/api/notification/isRead', { notificationId: item._id }, { withCredentials: true }).then(res => {
+    await axios.put('https://globalconnectfinalproject.onrender.com/api/notification/isRead', { notificationId: item._id }, { withCredentials: true }).then(res => {
       if (item.type == "comment") {
         navigate(`/profile/${ownData?._id}/activities/${item.postId}`)
       } else {
