9bc3f83cbc7db1dd2f4d97a77bfa52faff02d868
aadeshpandey1011 <2002.aman.p@gmail.com>
2025-08-13 00:14:41 +0530

    Update socket.js

--- Global-Connect-frontend/socket.js
+++ Global-Connect-frontend/socket.js
@@ -1,6 +1,6 @@
 import {io} from "socket.io-client";
 
-const socket = io('wss://globalconnectfinalproject.onrender.com');
+const socket = io('https://globalconnectfinalproject.onrender.com');
 
 
 export default socket;
