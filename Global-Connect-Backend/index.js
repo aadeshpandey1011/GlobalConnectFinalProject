a5776aa309a1364f1896d1292ea621641f7e3aa2
aadeshpandey1011 <2002.aman.p@gmail.com>
2025-08-12 22:50:31 +0530

    dummy vercel link

--- Global-Connect-Backend/index.js
+++ Global-Connect-Backend/index.js
@@ -15,7 +15,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:5173'
+  origin:[ 'http://localhost:5173',
+          'https://your-frontend-domain.vercel.app']
   
 }));
 
