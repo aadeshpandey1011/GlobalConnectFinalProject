1a819bd91cc8cb9fde2d5685f5eb000cb786c1fe
aadeshpandey1011 <2002.aman.p@gmail.com>
2025-08-12 23:38:42 +0530

    Update myNetwork.jsx with link

--- Global-Connect-frontend/src/pages/MyNetwork/myNetwork.jsx
+++ Global-Connect-frontend/src/pages/MyNetwork/myNetwork.jsx
@@ -15,7 +15,7 @@ const MyNetwork = () => {
     }
 
     const fetchFriendList = async () => {
-        await axios.get('http://localhost:4000/api/auth/friendsList', { withCredentials: true }).then((res) => {
+        await axios.get('https://globalconnectfinalproject.onrender.com/api/auth/friendsList', { withCredentials: true }).then((res) => {
             console.log(res);
             setData(res.data.friends)
             
@@ -26,7 +26,7 @@ const MyNetwork = () => {
     }
 
     const fetchPendingRequest = async () => {
-        await axios.get('http://localhost:4000/api/auth/pendingFriendsList', { withCredentials: true }).then((res) => {
+        await axios.get('https://globalconnectfinalproject.onrender.com/api/auth/pendingFriendsList', { withCredentials: true }).then((res) => {
             console.log(res);
             setData(res.data.pendingFriends)
         }).catch(err => {
