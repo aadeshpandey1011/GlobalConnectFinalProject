a6bb8b4fe7c73b2e7cf7d7d966b1769f2d7acb6c
aadeshpandey1011 <2002.aman.p@gmail.com>
2025-08-12 23:19:43 +0530

    Update googleLoginComp.jsx with link

--- Global-Connect-frontend/src/components/GoogleLogin/googleLoginComp.jsx
+++ Global-Connect-frontend/src/components/GoogleLogin/googleLoginComp.jsx
@@ -8,7 +8,7 @@ const GoogleLoginComp = (props) => {
     const navigate= useNavigate();
     const handleOnSucess = async (credResponse) => {
         const token = credResponse.credential;
-        const res = await axios.post('http://localhost:4000/api/auth/google', { token }, { withCredentials: true });
+        const res = await axios.post('https://globalconnectfinalproject.onrender.com/api/auth/google', { token }, { withCredentials: true });
 
         localStorage.setItem('isLogin', 'true');
         localStorage.setItem("userInfo", JSON.stringify(res.data.user));
