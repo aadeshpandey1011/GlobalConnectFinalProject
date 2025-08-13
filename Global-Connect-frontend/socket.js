bce76dad30b09ea808d8317e4b1ff1915c45d4b2
aadeshpandey1011 <2002.aman.p@gmail.com>
2025-08-13 10:40:52 +0530

    Update socket.js

--- Global-Connect-frontend/socket.js
+++ Global-Connect-frontend/socket.js
@@ -11,7 +11,7 @@
 import { io } from "socket.io-client";
 
 const BACKEND_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:10000";
+  import.meta.env.VITE_API_URL || "http://localhost:4000";
 
 console.log("Connecting to backend:", BACKEND_URL);
 
