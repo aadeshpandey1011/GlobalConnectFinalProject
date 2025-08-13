34b9e84749b5a4af26be747bcfad7b3bc7ad9e19
aadeshpandey1011 <2002.aman.p@gmail.com>
2025-08-13 11:24:14 +0530

    Update main.jsx

--- Global-Connect-frontend/src/main.jsx
+++ Global-Connect-frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.production.VITE_API_URL}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_API_URL}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
